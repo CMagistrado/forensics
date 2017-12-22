1ddabd7456e85dea544d492c67921883b5fa1ed6
Chris Magistrado <cmagistrado@gmail.com>
2017-12-22 04:34:49 -0800

    Takes a snapshot of RAM on a Mac OS X.
    Tested on:
    Macbook Air 10.12.6

---
+++ snap.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Create directories
+mkdir /tmp/mem
+mkdir /tmp/mem/Memory_Captures
+
+# Takes us to our work env
+cp osxpmem_2.0.1.zip /tmp/mem
+cd /tmp/mem
+unzip osxpmem_2.0.1.zip
+
+# Required to use utilites
+sudo chown -R root:wheel osxpmem.app/
+sudo osxpmem.app/osxpmem -o Memory_Captures/mem.aff4
+sudo osxpmem.app/osxpmem -e /dev/pmem -o Memory_Captures/mem.raw Memory_Captures/mem.aff4
+
+# Unload our kernel extension
+sudo osxpmem.app/osxpmem -u
